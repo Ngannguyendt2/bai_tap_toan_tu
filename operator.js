73fb5938671e0430af7256a14d949a8a21b4dda6
= <=>
2019-07-30 16:45:40 +0700

    up file bai tap toan tu

---
+++ operator.js
@@ -0,0 +1,9 @@
+//function oper() {
+    let width;
+    let height;
+    let area;
+    width=parseInt(prompt("width: "));
+    height=parseInt(prompt("height "));
+    area=width*height;
+    document.write("area: "+area);
+//}
